102f5416357a3369cbd4af6000279ded8b885afc
rahul6382 <rahulrahe999@gmail.com>
2020-11-25 18:20:10 +0530

    cricket-game

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody ball;
+
+    void Start()
+    {
+        ball = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.forward * 0.1f);
+        
+    }
+}
